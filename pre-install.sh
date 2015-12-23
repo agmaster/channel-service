c795d4863085f237515ea12af6a9aaee8abaad97
Hua Zhang <billyzhang2010@gmail.com>
2015-12-23 15:45:53 -0800

    update dependency 3rd packages

---
+++ pre-install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# install Godep
+go get -v github.com/tools/godep
+
+godep version
+
+echo "go get -v dependency packages"
+go get -v github.com/astaxie/beego/logs
+go get -v github.com/cactus/go-statsd-client/statsd
+go get -v github.com/julienschmidt/httprouter
+go get -v gopkg.in/mgo.v2
+go get -v gopkg.in/olivere/elastic.v2
+
+
